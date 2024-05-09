e824d29f37588317ea98f1e508e5ae7b9c8f1be9
Aditya SIngh <adityasingh161020003@gmail.com>
2024-05-09 09:44:04 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+let buttons = document.querySelectorAll(".button")
+let imput = document.querySelectorAll("input")
+let reset = document.querySelector("#reset")
+
+
+
+let string = ""
+ 
+
+Array.from(buttons).forEach((elements)=>{
+  elements.addEventListener("click",()=>{
+    // console.log(elements.innerHTML)
+
+    if(elements.innerHTML == "="){
+        string = eval(string)
+        string  = elements.innerHTML + string
+        document.querySelector(".input").value =  string
+    }
+
+    else if(elements.innerHTML == "C"){
+        string = ""
+        document.querySelector(".input").value =  string
+
+    }
+    else{
+
+        console.log(elements)
+        string = string + elements.innerHTML
+        // imput.value = string
+        document.querySelector(".input").value  =  string
+
+
+    }
+  })
+})
+
+reset.addEventListener('click',()=>{
+    string = ""
+    document.querySelector("input").value  = string
+})
+
+
+
+
+
+
+
+
+
+
